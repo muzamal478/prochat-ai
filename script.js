76fdb1925c294c56fb65fac9d9dfc4c9d567d9cd
Muzamal Asghar <127825801+muzamal478@users.noreply.github.com>
2025-05-11 17:14:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const newChatBtn = document.getElementById("new-chat-btn");
 const searchChat = document.getElementById("search-chat");
 
 // API Configuration
-const API_KEY = "AIzaSyAlOazVdBzeebrjvw_HClXTk5aoEDE7dxw"; // Replace this!
+const API_KEY = ""; // Replace this!
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 // Global Variables
